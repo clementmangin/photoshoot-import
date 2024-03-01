c2dd78e97f6ee7923cad366cfa05f79c6b76ac3c
Clément Mangin <clement.mangin@pm.me>
2024-02-29 23:51:42 -0500

    Initial Commit

---
+++ Photoshoot Import/Photoshoot_ImportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photoshoot_ImportApp.swift
+//  Photoshoot Import
+//
+//  Created by Clément Mangin on 2024-02-29.
+//
+
+import SwiftUI
+
+@main
+struct Photoshoot_ImportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
